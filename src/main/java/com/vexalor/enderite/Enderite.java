59b70e1b3b5873239a598e844f33a563517bf5b6
Roblortex <samuelwfortier@hotmail.com>
2024-10-04 17:23:06 -0400

    added features

--- src/main/java/com/vexalor/enderite/Enderite.java
+++ src/main/java/com/vexalor/enderite/Enderite.java
@@ -1,5 +1,6 @@
 package com.vexalor.enderite;
 
+import com.vexalor.enderite.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
@@ -11,6 +12,6 @@ public class Enderite implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-
+		ModItems.registerModItems();
 	}
 }
