74e44c8a8ca3f82f1568c05e197b5eb66d5cae5d
Roblortex <samuelwfortier@hotmail.com>
2024-10-04 16:54:16 -0400

    Initial commit

---
+++ src/main/java/com/vexalor/enderite/Enderite.java
@@ -0,0 +1,16 @@
+package com.vexalor.enderite;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Enderite implements ModInitializer {
+	public static final String MOD_ID = "enderite";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/com/vexalor/enderite/EnderiteClient.java
@@ -0,0 +1,10 @@
+package com.vexalor.enderite;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class EnderiteClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
